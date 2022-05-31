33a728f8cac1be91a8a3730c5b21b38953c7ff88
Abel_PC <569879601@qq.com>
2022-06-01 07:26:33 +0800

    计算时间函数；

--- UtilitiesLibrary/DateTimeUtilites/C-styleDateTimeUtilites/difftime.cpp
+++ UtilitiesLibrary/DateTimeUtilites/C-styleDateTimeUtilites/difftime.cpp
@@ -23,8 +23,15 @@ std::string sec2str(double sec) {
 }
 
 int main(int argn, char* argc[]) {
-	auto start = str2time_t("2022-03-17 00:00:00");
-	auto end = str2time_t("2022-06-01 00:00:00");
+	// 上海市封城 76天
+	// 2022-03-17 00:00:00
+	// 2022-06-01 00:00:00
+	if (argn < 3) {
+		std::cout << "usage> difftime \"2022-03-17 00:00:00\" \"2022-06-01 00:00:00\"" << std::endl;
+		return 1;
+	}
+	auto start = str2time_t(argc[1]);
+	auto end = str2time_t(argc[2]);
 	auto sec = std::difftime(end, start);
 	std::cout << sec2str(sec);
 	return 0;
