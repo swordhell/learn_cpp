8ba94afac395d6646c97ea16e12d4f34d3e0ca53
abelhomeubuntu <swordhell@126.com>
2022-08-04 08:33:39 +0800

    Item M12：理解“抛出一个异常”与“传递一个参数”或“调用一个虚函数”间的差异

---
+++ MoreEffectiveCPP/ItemM12/ItemM12.cpp
@@ -0,0 +1,43 @@
+// g++ -std=c++17 -g -fsanitize=address ItemM12.cpp -o ../bin/ItemM12
+#include <iostream>
+
+class Widget {
+public:
+  Widget(const Widget& o) : widgetSize_(o.widgetSize_) {std::cout << "Widget(const Widget& o)" << std::endl;}
+  Widget(int widgetSize) : widgetSize_(widgetSize) {std::cout << "Widget(int widgetSize)" << std::endl;}
+  ~Widget() { std::cout << "~Widget()..." << std::endl; }
+  int widgetSize_;
+};
+
+class SpecialWidget : public Widget {
+public:
+  SpecialWidget() : Widget(10), specialWidgetSize_(8) {std::cout << "SpecialWidget()..." << std::endl;}
+  ~SpecialWidget() { std::cout << "~SpecialWidget()..." << std::endl; }
+  int specialWidgetSize_;
+};
+
+void WillThrow() {
+    SpecialWidget localSpecialWidget;
+    Widget& rw = localSpecialWidget;
+    if (localSpecialWidget.specialWidgetSize_ == 8) {
+        throw rw;
+    }
+}
+
+void Catch2() {
+    try {
+        WillThrow();
+    } catch (Widget& w) {
+        std::cout << "Caught Widget" << std::endl;
+        throw w;
+    }
+}
+
+int main(int argn, char *argc[]) {
+    try {
+        Catch2();
+    } catch (Widget& w) {
+        std::cout << "Caught Widget" << std::endl;
+    }
+  return 0;
+}
