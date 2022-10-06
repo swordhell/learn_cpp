fac90a4732bc7a930139ca2c29400d7daff63d32
abelhomeubuntu <swordhell@126.com>
2022-10-06 08:14:26 +0800

    备注多重继承相关知识；

--- Base/Inherit/Inherit.cpp
+++ Base/Inherit/Inherit.cpp
@@ -9,6 +9,7 @@ class A
 public:
 	A() { a = 0; }
 	int a;
+	char buff[100]; // 我们将A中填充一些内存块，将会看到使用非虚拟继承的类，将会膨胀的很厉害。
 	virtual void fun() { std::cout << "A::fun()\n"; };
 };
 
@@ -91,20 +92,17 @@ operator << (ostream& os, const Point<type, dim>& pt)
 
 int main(int argn, char* argc[]) {
 	std::cout << "Hello the world!" << std::endl;
-	A a;
-	B b;
-	C c;
-	D d;
-	B1 b1;
-	C1 c1;
-	D1 d1;
-
+	A a; // {_vptr$A = 0x4020a0 <vtable for A+16>, a = 0}
 	// 多态是使用 virtual function table + virtual pointer
 
-	std::cout << "a " << sizeof(a) << std::endl;//12 int(4) + 虚表指针：vfptr 8 byte
-	std::cout << "b " << sizeof(b) << std::endl;//16 = 一个完整的A 12 + int(4) 
-	std::cout << "c " << sizeof(c) << std::endl;// 16 = 一个完整的A 12 + int(4) 
-	std::cout << "d " << sizeof(d) << std::endl;//32 = b和c拥有独立的A指针，这个里面没有缓存一个A对象
+	B b; // {_vptr$A = 0x4020d0 <vtable for B+16>, a = 2}, b = 2}
+	C c; // {_vptr$A = 0x402108 <vtable for C+16>, a = 3}, c = 3}
+	D d; // {<B> = {<A> = {_vptr$A = 0x402140 <vtable for D+16>, a = 2}, b = 2}, <C> = {<A> = {_vptr$A = 0x402158 <vtable for D+40>, a = 3}, c = 3}, <No data fields>}
+	std::cout << "a " << sizeof(a) << std::endl;//12 = 虚表指针：_vptr$A 8 byte + int(4)
+	std::cout << "b " << sizeof(b) << std::endl;//16 = 虚表指针：_vptr$A 8 byte + A::a int(4) + B::b int(4) 
+	std::cout << "c " << sizeof(c) << std::endl;//16 = 虚表指针：_vptr$A 8 byte + A::a int(4) + C::c int(4) 
+	std::cout << "d " << sizeof(d) << std::endl;//32 = 虚表指针：_vptr$A 8 byte + B::A::a int(4) + B::b int(4) + _vptr$A 8 byte + C::A::a int(4) + C::c int(4)
+	// b和c拥有独立的A对象，互相不共享，意味着，如果想在d层级调用A变量需要指定基类。
 	d.fun();
 
 	//std::cout << "d.a" << d.a << std::endl;// 这样会有问题
@@ -113,14 +111,16 @@ int main(int argn, char* argc[]) {
 	std::cout << std::endl;
 
 	// virtual inherit 代表共享使用相同的A
-	std::cout << "b1 " << sizeof(b1) << std::endl;// 24 一个完整的A 12 + 自身的 vfptr 8 byte + int(4)
-	std::cout << "c1 " << sizeof(c1) << std::endl;// 24 一个完整的A 12 + 自身的 vfptr 8 byte + int(4)
-	std::cout << "d1 " << sizeof(d1) << std::endl;// 36 一个完整的A 12 + 完整的B1 12 完整的C1 12=36
+	B1 b1; // {<A> = {_vptr$A = 0x4021d8 <vtable for B1+56>, a = 22}, _vptr$B1 = 0x4021b8 <vtable for B1+24>, b = 22}
+	C1 c1; // {<A> = {_vptr$A = 0x402258 <vtable for C1+56>, a = 33}, _vptr$C1 = 0x402238 <vtable for C1+24>, c = 33}
+	D1 d1; // {<B1> = {<A> = {_vptr$A = 0x4022f8 <vtable for D1+88>, a = 33}, _vptr$B1 = 0x4022b8 <vtable for D1+24>, b = 22}, <C1> = {_vptr$C1 = 0x4022d8 <vtable for D1+56>, c = 33}, <No data fields>}
+	std::cout << "b1 " << sizeof(b1) << std::endl;// 24 = 虚表指针：_vptr$A 8 byte + a int(4) + _vptr$B1 8 byte + b int(4)
+	std::cout << "c1 " << sizeof(c1) << std::endl;// 24 = 虚表指针：_vptr$A 8 byte + a int(4) + _vptr$C1 8 byte + c int(4)
+	std::cout << "d1 " << sizeof(d1) << std::endl;// 36 = 虚表指针：_vptr$A 8 byte + a int(4) + _vptr$B1 8 byte + b int(4) + _vptr$C1 8 byte + c int(4)
 
-	std::cout << "d1.B1::a " << d1.B1::a << std::endl;
+	std::cout << "d1.B1::a " << d1.B1::a << std::endl; // 这两个值都会相同
 	std::cout << "d1.C1::a " << d1.C1::a << std::endl;
 	std::cout << std::endl;
 
-	
 	return 0;
 }
